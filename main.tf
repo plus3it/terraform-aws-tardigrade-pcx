13514128098c1ec808e47b46147213bc678c0511
Loren Gordon <lorengordon@users.noreply.github.com>
2020-12-01 12:56:00 -0800

    Cleans up tf syntax and simplifies module inputs

--- main.tf
+++ main.tf
@@ -1,37 +1,35 @@
-provider "aws" {
+provider aws {
   alias = "peer"
 }
 
-data "aws_vpc" "peer" {
+data aws_caller_identity peer {}
+
+data aws_vpc this {
+  id = var.vpc_id
+}
+
+data aws_vpc peer {
   provider = aws.peer
 
   id = var.peer_vpc_id
 }
 
-resource "aws_vpc_peering_connection" "this" {
-  peer_owner_id = var.peer_owner_id
+resource aws_vpc_peering_connection this {
+  peer_owner_id = data.aws_caller_identity.peer.account_id
   peer_vpc_id   = var.peer_vpc_id
   vpc_id        = var.vpc_id
-  tags = merge(
-    var.tags,
-    {
-      "Name" = "${format("%v", var.name)}-${format("%v", var.vpc_cidr)}<->${format("%v", var.peer_alias)}-${data.aws_vpc.peer.cidr_block}"
-    },
-  )
+  tags          = var.tags
 }
 
-resource "aws_vpc_peering_connection_accepter" "this" {
+resource aws_vpc_peering_connection_accepter this {
   provider = aws.peer
 
   vpc_peering_connection_id = aws_vpc_peering_connection.this.id
   auto_accept               = true
-
-  tags = {
-    Name = "${format("%v", var.peer_alias)}-${data.aws_vpc.peer.cidr_block}<->${format("%v", var.name)}-${format("%v", var.vpc_cidr)}"
-  }
+  tags                      = var.peer_tags
 }
 
-resource "aws_route" "public" {
+resource aws_route public {
   count = length(var.public_route_tables)
 
   route_table_id            = var.public_route_tables[count.index]
@@ -39,7 +37,7 @@ resource "aws_route" "public" {
   vpc_peering_connection_id = aws_vpc_peering_connection.this.id
 }
 
-resource "aws_route" "private" {
+resource aws_route private {
   count = length(var.private_route_tables)
 
   route_table_id            = var.private_route_tables[count.index]
@@ -47,15 +45,14 @@ resource "aws_route" "private" {
   vpc_peering_connection_id = aws_vpc_peering_connection.this.id
 }
 
-resource "aws_route" "peer" {
+resource aws_route peer {
   count = length(var.peer_route_tables)
 
   provider = aws.peer
 
   route_table_id            = var.peer_route_tables[count.index]
-  destination_cidr_block    = var.vpc_cidr
+  destination_cidr_block    = data.aws_vpc.this.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection_accepter.this.id
 
   depends_on = [aws_vpc_peering_connection_accepter.this]
 }
-
