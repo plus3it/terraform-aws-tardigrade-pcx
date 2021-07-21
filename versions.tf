252af1d8cfd42e3c464a8d765a103702adef9e3b
Karri Balk <kbalk@users.noreply.github.com>
2021-07-21 17:35:14 -0400

    refactor: Correct another terraform lint error

--- versions.tf
+++ versions.tf
@@ -5,7 +5,7 @@ terraform {
     aws = {
       source                = "hashicorp/aws"
       version               = ">= 3.0"
-      configuration_aliases = [ aws.peer ]
+      configuration_aliases = [aws.peer]
     }
   }
 }
